795b1778224729150b22aed52466804377e2d54b
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2021-03-21 13:07:31 +0100

    Initial commit

---
+++ ActiveDirectoryCreation/AzureAdConnect/ou.ps1
@@ -0,0 +1,8 @@
+param( 
+[parameter(Mandatory=$true)][string]$ouUsers,
+[parameter(Mandatory=$true)][string]$ouHosts
+)
+
+New-ADOrganizationalUnit -Name $ouUsers
+New-ADOrganizationalUnit -Name $ouHosts
+
